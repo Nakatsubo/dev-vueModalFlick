da336911290135849636d3be7fe5249187bedfa5
NakatsuboYusuke <>
2020-12-05 22:55:41 +0900

    [Fixed] HTML OUTLINE

--- src/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-var date = new Date();
-var element = document.getElementById('time');
-
-function getTime(date){
-    return data.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-}
-//returnの最初のdateをわざとdataと間違えて記述してみました。
-element.innerHTML = getTime(date);
